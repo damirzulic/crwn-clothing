77d99858c964c3d1adf963710e04e8d2fd054db4
DammirZ <dmair.zulic@gmail.com>
2020-12-13 18:39:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomePage from './hompage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
