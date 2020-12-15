1b9db89fe14596ab1583ae5c825e899c968c6ef3
DammirZ <dmair.zulic@gmail.com>
2020-12-15 01:01:32 +0100

    4 korak zavrsen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import HomePage from './pages/homepage/hompage.component';
+import { Switch, Route } from "react-router-dom";
 
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      <div>
+        <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        </Switch>
       </div>
     );
   }
